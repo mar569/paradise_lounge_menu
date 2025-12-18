3c105b25d8eb42f1e369f464ac45f1736b8bd468
mar2025 <efimin01@icloud.com>
2025-12-18 23:54:20 +0300

    scroll

--- src/components/PageLayout.tsx
+++ src/components/PageLayout.tsx
@@ -3,12 +3,14 @@ import MobileBottomNav from './MobileBottomNav';
 
 export default function PageLayout({ children }: { children: React.ReactNode }) {
     return (
-        <>
-            <div className="relative min-h-screen flex flex-col">
+        <div className="relative min-h-screen flex flex-col" style={{ height: '100vh' }}>
+            <main className="flex-1" style={{ height: '90vh', overflow: 'auto' }}>
+                {children}
+            </main>
 
-                <main className="flex-1">{children}</main>
+            <div style={{ height: '10vh', flexShrink: 0 }}>
                 <MobileBottomNav />
             </div>
-        </>
+        </div>
     );
 }
